ab6ec76e973e65a69d7d71f581c9b2d65d89659d
SnehalSonawane24 <snehalsonawane242002@gmail.com>
2024-02-18 12:53:43 +0530

    first commit

---
+++ src/moviesData.js
@@ -0,0 +1,101 @@
+export const movies = [{
+    title: "The Avengers",
+    plot: "Plot of the movie is related to super natural powers",
+    price: 199,
+    rating: 8.0,
+    poster: "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+    stars: 0,
+    fav:false,
+    isInCart:false
+    },
+
+    {
+    title: "Ironman",
+    plot: "After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.",
+    price: 399,
+    rating: 8.9,
+    poster: "https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p170620_p_v8_az.jpg",
+    stars: 0,
+    fav:false,
+    isInCart:false
+    },
+    {
+      
+        title: 'The Dark Knight',                  
+        plot:
+          'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
+        poster:
+          'https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg',                  
+        rating: '9.0',            
+        price: 199,            
+        stars: 0,            
+        fav: false,
+        isInCart: false                  
+      },
+      {
+       
+        title: 'Captain Marvel',                  
+        plot:
+          'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
+        poster:
+          'https://rukminim2.flixcart.com/image/850/1000/jruyikw0/poster/h/c/4/medium-captain-marvel-4-captain-marvel-poster-for-room-office-13-original-imafdk63jzj3th6f.jpeg',                  
+        rating: '9.0',            
+        price: 199,            
+        stars: 0,            
+        fav: false,
+        isInCart: false                  
+      },
+      {
+        
+        title: 'The Thor',                  
+        plot:
+          'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
+        poster:
+          'https://www.shutterstock.com/blog/wp-content/uploads/sites/5/2022/07/Thor-Love-and-Thunder-1.jpeg',                  
+        rating: '9.0',            
+        price: 199,            
+        stars: 0,            
+        fav: false,
+        isInCart: false                  
+      },
+      {
+        id:2,
+        title: 'Hulk',                  
+        plot:
+          'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
+        poster:
+          'https://pbs.twimg.com/media/FIsm9GwXEAMI-sT.jpg',                  
+        rating: '9.0',            
+        price: 199,            
+        stars: 0,            
+        fav: false,
+        isInCart: false                  
+      },
+      {
+        id:2,
+        title: 'Spiderman',                  
+        plot:
+          'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
+        poster:
+          'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/94ab2ad3-e0d4-4764-85ad-873af742bd4a/ddhhb1n-7fb57059-dad7-4499-90fa-95ea591ad8e8.png/v1/fit/w_375,h_577,q_70,strp/spider_man_clone_conspiracy_movie_poster_by_arkhamnatic_ddhhb1n-375w.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzk0YWIyYWQzLWUwZDQtNDc2NC04NWFkLTg3M2FmNzQyYmQ0YVwvZGRoaGIxbi03ZmI1NzA1OS1kYWQ3LTQ0OTktOTBmYS05NWVhNTkxYWQ4ZTgucG5nIiwiaGVpZ2h0IjoiPD0yNDYxIiwid2lkdGgiOiI8PTE2MDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uud2F0ZXJtYXJrIl0sIndtayI6eyJwYXRoIjoiXC93bVwvOTRhYjJhZDMtZTBkNC00NzY0LTg1YWQtODczYWY3NDJiZDRhXC9hcmtoYW1uYXRpYy00LnBuZyIsIm9wYWNpdHkiOjk1LCJwcm9wb3J0aW9ucyI6MC40NSwiZ3Jhdml0eSI6ImNlbnRlciJ9fQ.dFWeUyx8w8J3RDPzeQcXy1gU24MQrAzirRTy_7Yez18',                  
+        rating: '9.0',            
+        price: 199,            
+        stars: 0,            
+        fav: false,
+        isInCart: false                  
+      },
+      {
+        id:2,
+        title: 'Captain America',                  
+        plot:
+          'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
+        poster:
+          'https://static.displate.com/162x227/displate/2021-01-28/bce221eff0de2ed033e89311c45c805e_24f94cda8031146d198a8636239c55f5.jpg',                  
+        rating: '9.0',            
+        price: 199,            
+        stars: 0,            
+        fav: false,
+        isInCart: false                  
+      },
+
+]
